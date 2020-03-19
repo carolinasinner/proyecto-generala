246fbe5a7235974cd9fe73aea289f42a3ac6a9ae
carolinasinner <carosinner1994@hotmail.com>
2020-03-19 00:17:14 -0300

    primera version

---
+++ www/js/routes.js
@@ -0,0 +1,17 @@
+
+var routes = [
+  {
+    path: '/',
+    url: './index.html',
+  },
+  {
+    path: '/anotador/',
+    url: './pages/anotador.html',
+  },
+
+  // Default route (404 page). MUST BE THE LAST
+  {
+    path: '(.*)',
+    url: './pages/404.html',
+  },
+];
